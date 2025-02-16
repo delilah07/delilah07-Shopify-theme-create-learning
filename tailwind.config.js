22651a5b59cd7a6aab9de062d614cecf28dce742
Iryna Abrosimova <iryna.abrosimova@cxdisruption.com>
2025-02-16 10:54:30 +0100

    create shopify theme and add header

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./**/*.liquid'], // Ensure it scans Shopify liquid files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    hover: true, // Ensure hover effects are applied
+  },
+};
